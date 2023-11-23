c6499b6d5ca9cc8a81a92eb62d87e6cc4333d8fe
topurayhan007 <topurayhan007@gmail.com>
2023-11-24 00:58:10 +0600

    express.Router()

--- project-1/dist/app/app.js
+++ project-1/dist/app/app.js
@@ -9,6 +9,29 @@ const app = (0, express_1.default)();
 // parsers
 app.use(express_1.default.json());
 app.use(express_1.default.text());
+// Router
+const userRouter = express_1.default.Router();
+const courseRouter = express_1.default.Router();
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/courses", courseRouter);
+userRouter.post("/create-user", (req, res) => {
+    const user = req.body;
+    console.log(user);
+    res.json({
+        success: true,
+        message: "User created successfully",
+        data: user,
+    });
+});
+courseRouter.post("/create-course", (req, res) => {
+    const user = req.body;
+    console.log(user);
+    res.json({
+        success: true,
+        message: "Course created successfully",
+        data: user,
+    });
+});
 // middleware
 const logger = (req, res, next) => {
     console.log(req.url, req.method, req.hostname);
--- project-1/src/app/app.ts
+++ project-1/src/app/app.ts
@@ -6,6 +6,34 @@ const app = express();
 app.use(express.json());
 app.use(express.text());
 
+// Router
+const userRouter = express.Router();
+const courseRouter = express.Router();
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/courses", courseRouter);
+
+userRouter.post("/create-user", (req: Request, res: Response) => {
+  const user = req.body;
+  console.log(user);
+
+  res.json({
+    success: true,
+    message: "User created successfully",
+    data: user,
+  });
+});
+
+courseRouter.post("/create-course", (req: Request, res: Response) => {
+  const user = req.body;
+  console.log(user);
+
+  res.json({
+    success: true,
+    message: "Course created successfully",
+    data: user,
+  });
+});
+
 // middleware
 const logger = (req: Request, res: Response, next: NextFunction) => {
   console.log(req.url, req.method, req.hostname);
